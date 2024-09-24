bea693c5de3d76fb22fdf9c26256bd8c9fa1b88f
ja-Reeve <82411887+ja-Reeve@users.noreply.github.com>
2024-09-24 16:05:57 +1000

    Create 2.1.read_mapping.sh
    Updated read mapping script. The BAM sorting and duplicate removal are moved to separate scripts.

---
+++ 2.read_mapping/2.1.read_mapping.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+### Step 2.1 of short read processing pipeline
+### Read mapping using BWA. This is a very time consuming step, 
+### it may take over a day for the reads to map.
+### This script was designed on a SBATCH system.
+
+### James Reeve
+### 2024-09-24
+
+### Job parameters
+#SBATCH -n 20
+#SBATCH -t 2-0:00:00
+#SBATCH -J Lsrp_tut_2.1
+#SBATCH --output logfiles/Lsrp_read_map.out
+#SBATCH --error logfiles/Lsrp_read_map.err
+
+### Software
+module load bwa/0.7.18
+module load samtools/1.20
+
+### File path
+DIR=/path/to/testing/envrionment
+REF=/path/to/reference/genome
+
+### Script
+# 1) Index reference genome
+# Note: this only needs to be run once
+bwa index $REF/genome.fasta
+
+# 2) Mapping
+bwa mem \
+        -M -t 20 \
+        -R '@RG\tID:CZA020_Ls\tSM:CZA020_Ls\tPL:illumina' \
+        $REF/genome.fasta \
+        $DIR/CZA020_Ls_R1.fastq.gz \
+        $DIR/CZA020_Ls_R2.fastq.gz |\ # pipe to samtools to compress into BAM
+samtools view -b > $DIR/CZA020_Ls_PE.bam
